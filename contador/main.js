303c239af8d40a9b78b613fae173fcd32c0fa0f1
samilila1d <106823855+samilila1d@users.noreply.github.com>
2022-10-05 12:53:12 -0400

    Add files via upload

---
+++ contador/main.js
@@ -0,0 +1,11 @@
+let inputTextArea = document.getElementById("input-textarea");
+let characCount = document.getElementById("charac-count");
+let wordCount = document.getElementById("word-count");
+let rowCount = document.getElementById("row-count");
+
+inputTextArea.addEventListener("input", () => {
+  characCount.textContent = inputTextArea.value.length;
+  let txt = inputTextArea.value.trim();
+  wordCount.textContent = txt.split(/\s+/).filter((item) => item).length;
+  rowCount.textContent = txt.split(/\n/).filter((item) => item).length;
+});
