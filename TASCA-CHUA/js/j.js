f831beb3510ab08c194ecf1f954527d469e7fea5
LEUMIS <97970039+LEUMIS@users.noreply.github.com>
2022-10-10 01:41:23 -0400

    Add files via upload

--- TASCA-CHUA/js/j.js
+++ TASCA-CHUA/js/j.js
@@ -24,3 +24,54 @@ window.addEventListener('scroll', function()  {
         }
       }
   });
+
+
+
+
+
+
+
+  
+const click = new Audio('./assets/mp3/ring.mp3')
+const clock = document.querySelector(".clock");
+const button = document.querySelector(".button");
+
+let hours24 = false;
+let time = "AM";
+
+const engineClock = ()=>{
+    setInterval(getCurrentTime,1000);
+}
+
+const getCurrentTime = ()=>{
+    const date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    if(!hours24){
+        time = hours > 12 ? "PM" : "AM";
+        hours = hours > 12 ? hours - 12 : hours;
+        hours = hours == 0 ? 12 : hours;
+    } else {time="";
+
+    }
+
+    clock.innerHTML = `${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(seconds)} ${time}`;
+}
+
+const formatNumber = n =>{
+    return n < 10 ? `0${n}` : n;
+}
+
+button.addEventListener("click",e=>{
+    e.preventDefault();
+    click.play();
+    hours24 = !hours24;
+  
+    getCurrentTime();
+})
+
+
+// getCurrentTime();
+engineClock();
